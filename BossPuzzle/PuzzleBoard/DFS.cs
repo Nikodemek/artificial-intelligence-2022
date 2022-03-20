2fca866407d27e11947620c9c8d69186746e404d
trela677 <236677@edu.p.lodz.pl>
2022-03-20 15:59:05 +0100

    Re.... re... re. aa

--- BossPuzzle/PuzzleBoard/DFS.cs
+++ BossPuzzle/PuzzleBoard/DFS.cs
@@ -7,9 +7,11 @@ public class DFS : IPuzzleSolver
 {
     private readonly int _depth;
     private readonly Dir[] _directions;
+    private HashSet<ulong> visited;
+    private int _recursionDepth;
 
     public DFS(Dir[] directions)
-        : this(directions, 100)
+        : this(directions, 20)
     { }
 
     public DFS(Dir[] directions, int depth)
@@ -18,41 +20,77 @@ public class DFS : IPuzzleSolver
                             .Reverse()
                             .ToArray();
         _depth = depth;
+        visited = new HashSet<ulong>();
+        _recursionDepth = 0;
     }
 
-    public Board Solve(in Board board)
+    /*public Board Solve(in Board board)
     {
         if (board.IsValid()) return board;
 
         var visited = new HashSet<ulong>();
         var stack = new Stack<Board>();
+        var recursionDepth = 0;
 
         stack.Push(board);
 
         while (stack.Count > 0)
         {
-            if (stack.Count > _depth)
-            {
-                stack.Pop();
-            }
+            Console.WriteLine(recursionDepth);
+            Console.WriteLine(stack.Count);
 
             var currentBoard = stack.Pop();
-            // this is the only place that should define board as visited
-            if (!visited.Add(currentBoard.Hash)) continue;
             
+            // this is the only place that should define board as visited
+            if (!visited.Add(currentBoard.Hash))
+            {
+                recursionDepth--;
+                continue;
+            }
+
+            if (recursionDepth > _depth)
+            {
+                continue;
+            }
+
             var directions = currentBoard.ClarifyMovement(_directions);
 
             foreach (var direction in directions)
             {
                 var nextBoard = currentBoard.Move(direction);
                 nextBoard.AddToPath(direction);
-                
+            
                 if (nextBoard.IsValid()) return nextBoard;
-                
+            
                 if (!visited.Contains(nextBoard.Hash)) stack.Push(nextBoard);
             }
+
         }
 
         return board;
+    }*/
+    
+    public Board Solve(in Board board)
+    {
+        if (board.IsValid() || _recursionDepth > _depth) return board;
+        
+        _recursionDepth++;
+
+        visited.Add(board.Hash);
+
+        var directions = board.ClarifyMovement(_directions);
+
+        foreach (var direction in directions)
+        {
+            var nextBoard = board.Move(direction);
+            nextBoard.AddToPath(direction);
+        
+            if (nextBoard.IsValid()) return nextBoard;
+
+            if (!visited.Contains(nextBoard.Hash)) return Solve(nextBoard);
+        }
+        
+        return board;
     }
+    
 }
