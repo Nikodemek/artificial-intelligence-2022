686c430194e31fcdfe50cd890edccf378b1877b0
trela677 <236677@edu.p.lodz.pl>
2022-03-20 01:11:52 +0100

    DFS probably implemented.

--- BossPuzzle/Program.cs
+++ BossPuzzle/Program.cs
@@ -8,7 +8,7 @@ class Program
 {
     public static void Main()
     {
-        var readFile = new FileFifteenPuzzleDao("test.file");
+        /*var readFile = new FileFifteenPuzzleDao("test.file");
         Board board = readFile.Read();
         board.Print();
         
@@ -23,6 +23,24 @@ class Program
         var solvedBoard = board.Solve(bfsUDLR);
         solvedBoard.Print();
 
+        var saveFile = new FileFifteenPuzzleDao("test_sol.file");
+        saveFile.Write(solvedBoard);*/
+        
+        var readFile = new FileFifteenPuzzleDao("test.file");
+        Board board = readFile.Read();
+        board.Print();
+        
+        var dfsUDLR = new DFS(new[]
+        {
+            Dir.Up,
+            Dir.Down,
+            Dir.Left,
+            Dir.Right
+        });
+        
+        var solvedBoard = board.Solve(dfsUDLR);
+        solvedBoard.Print();
+        
         var saveFile = new FileFifteenPuzzleDao("test_sol.file");
         saveFile.Write(solvedBoard);
 
---
+++ BossPuzzle/PuzzleBoard/DFS.cs
@@ -0,0 +1,51 @@
+﻿using BossPuzzle.Utils;
+
+namespace BossPuzzle.PuzzleBoard;
+using Dir = Board.Direction;
+
+public class DFS: IPuzzleSolver
+{
+    private readonly Dir[] _directions;
+    
+    public DFS(Dir[] directions)
+    {
+        _directions = Cloner.SingleArr(directions);
+    }
+
+    public Board Solve(Board board)
+    {
+        if (board.IsValid()) return board;
+
+        var visited = new HashSet<ulong>();
+        var stack = new Stack<Board>();
+
+        stack.Push(board);
+
+        while (stack.Count > 0)
+        {
+            if (stack.Count > 20)
+            {
+                stack.Pop();
+            }
+            
+            var currentBoard = stack.Pop();
+            visited.Add(board.Hash);
+            var directions = currentBoard
+                                    .ClarifyMovement(_directions)
+                                    .Reverse()
+                                    .ToArray();
+
+            foreach (var direction in directions)
+            {
+                var nextBoard = currentBoard.Move(direction);
+                nextBoard.AddToPath(direction);
+
+                if (nextBoard.IsValid()) return nextBoard;
+
+                if (visited.Add(nextBoard.Hash)) stack.Push(nextBoard);
+            }
+        }
+
+        return board;
+    }
+}
